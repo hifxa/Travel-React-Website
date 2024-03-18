93146096a845148b3c052f2e378fcf58814e573e
Hamza <hamza.official010@gmail.com>
2024-03-18 18:58:42 +0530

    frontend changes using: react, hook, DOM

---
+++ src/Components/pages/Ratings.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+import Footer from '../Footer';
+
+export default function Ratings() {
+    return (
+        <>
+            <h1 className='ratings'>
+                Ratings
+            </h1>
+            <Footer />
+        </>
+    );
+}
---
+++ src/Components/pages/SignUp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../App.css';
+import Footer from '../Footer';
+export default function SignUp() {
+    return (
+        <>
+            <h1 className='sign-up'>
+                SignUp
+            </h1>
+            <Footer />
+        </>
+    );
+}
